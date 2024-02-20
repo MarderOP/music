5a1d56658f1f0f3aecc81c73e503183f091e8b86
MarderOP <sebastiankuranowski@gmail.com>
2024-02-20 08:00:33 +0100

    Adding basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import MusicComponent from './MusicComponent';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./MainComponents/Home";
 function App() {
-  let Music={
-    Chords: [
-      { Name: 'C', Path: [0, 1, 0, 2, 3, 0] },
-      { Name: 'G', Path: [3, 2, 0, 0, 3, 3] },
-    ],
-    Song: [
-      { Play: 'C', Interval: 1000 },
-      { Play: 'G', Interval: 1000 },
-    ],
-    Capo:1
-  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <MusicComponent Music={Music}/>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={Home}/>
+        <Route path="home" element={Home}/>
+        <Route path="" element={Home}/>
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
