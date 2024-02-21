fda16e613bb5e2b91ad6fb7479696f5b8fc8e94a
MarderOP <116825677+MarderOP@users.noreply.github.com>
2024-02-21 11:11:21 +0000

    fixed router to direct to elements

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={Home}/>
-        <Route path="home" element={Home}/>
-        <Route path="" element={Home}/>
-
+        <Route path="/" element={<Home/>}/>
+        <Route path="/home" element={<Home/>}/>
+        <Route path="*" element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   );
