2b158c38761bf0fa1f21a1a3c3fa359fdeb74c08
MarderOP <116825677+MarderOP@users.noreply.github.com>
2024-02-28 09:32:18 +0000

    update sound system

---
+++ client/src/Components/TabChord.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles1 from "../css/frets.css";
+
+const Sounds = ['A', 'Ais', 'B', 'C', 'Cis', 'D', 'Dis', 'E', 'F', 'Fis', 'G', 'Gis'];
+
+const TabChord = () => {
+
+}
+
+export default TabChord;
--- client/src/MainComponents/Home.js
+++ client/src/MainComponents/Home.js
@@ -1,7 +1,10 @@
+import TabChord from "../Components/TabChord";
 const Home=()=>{
 return(
     <>
     <h1>Welcome</h1>
+    <h2>Create a Chord</h2>
+    <TabChord/>
     </>
 )
 }
